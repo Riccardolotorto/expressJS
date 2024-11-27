83d64465e46895bd580d34bdacf63139b2763f56
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-11-27 10:30:52 +0000

    funziona, con file gitpod

--- clientMode/client-app/src/app/app.component.ts
+++ clientMode/client-app/src/app/app.component.ts
@@ -8,15 +8,15 @@ import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 })
 export class AppComponent {
   title = 'client-app';
-  results!: any;
+  results: string[];
   // Inject HttpClient into your component or service.
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     // Make the HTTP request:
-    this.http.get('http://3000-riccardolotor-expressjs-or0v878upk0.ws-eu116.gitpod.io/api').subscribe(data => {
-      // Read the result field from the JSON response.
-      console.log(data);
-      this.results = data;
-    });
+    this.http.get('https://3000-riccardolotor-expressjs-or0v878upk0.ws-eu116.gitpod.io/api').subscribe(data => {
+    // Read the result field from the JSON response.
+    this.results = data['results'];
+    console.log(this.results);
+  });
   }
 }
